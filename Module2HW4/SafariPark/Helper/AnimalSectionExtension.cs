001d96cd6ab1ed5634e7cff00efeb39760a3dab5
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-22 19:21:35 +0200

    added AnimalSectionExtension

---
+++ Module2HW4/SafariPark/Helper/AnimalSectionExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SafariPark.Models;
+
+namespace SafariPark.Helper
+{
+    public static class AnimalSectionExtension
+    {
+        public static Animal[] FindAnimalsByGenus(this AnimalSection animalSection, string genus)
+        {
+            var animalsInSection = animalSection.Animals;
+            var sameSpeciesAnimals = new Animal[animalsInSection.Length];
+            var size = 0;
+            for (var i = 0; i < animalsInSection.Length; i++)
+            {
+                if (animalsInSection[i].Genus.Equals(genus))
+                {
+                    sameSpeciesAnimals[size++] = animalsInSection[i];
+                }
+            }
+
+            if (size != animalsInSection.Length)
+            {
+                Array.Resize(ref sameSpeciesAnimals, size);
+            }
+
+            return sameSpeciesAnimals;
+        }
+    }
+}
