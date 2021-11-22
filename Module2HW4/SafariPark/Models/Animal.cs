6e485992c2927e6a31bd2551b4e1803bfd18647f
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-22 19:22:52 +0200

    added Animal

---
+++ Module2HW4/SafariPark/Models/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace SafariPark.Models
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public abstract string Species { get; }
+        public abstract bool BodyTemperatureDependsOnEnvironment { get; }
+        public abstract string Genus { get; }
+    }
+}
